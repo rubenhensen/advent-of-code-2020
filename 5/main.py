af18d0722c31931c21bc87ff6aa2d11c6ecf6693
Ruben <ruben.hensen@gmail.com>
2020-12-06 17:00:36 +0100

    Add soltution 5.1

---
+++ 5/main.py
@@ -0,0 +1,55 @@
+import re
+
+parse_regex = re.compile("^(?P<rows>[BF]*)(?P<columns>[LR]*)")
+
+filename = '5/input.txt'
+highest_seat_number = 0
+
+def parse_input(line):
+        (row, column) = parse_regex.search(line).groups()
+        return (row, column)
+
+def read_file():
+    with open(filename) as file:
+        return file.readlines()
+
+def binary_search(pos):
+    total = 2**len(pos)
+    high = total -1
+    low = 0
+    for letter in pos:
+        # print('letter: ' + letter)
+        # print('high: ' + str(high))
+        # print('low: ' + str(low))
+        midpoint = (low + high) // 2
+        # print('midpoint: ' + str(midpoint))
+        # print('\n')
+        # Lower half
+        if letter == "F" or letter == "L":
+            if midpoint == low:
+                return low
+            high = midpoint
+        
+        # Higher half
+        if letter == "B" or letter == "R":
+            if midpoint == low:
+                return high
+            low = midpoint + 1
+
+def calc_seat_number(row, col):
+    return row * 8 + col
+
+
+if __name__ == "__main__":
+    input = read_file()
+    for seat in input:
+        # print("--------")
+        (binary_row, binary_col) = parse_input(seat)
+        row = binary_search(binary_row)
+        col = binary_search(binary_col)
+        # print("--------\n")
+        seat_number = calc_seat_number(row, col)
+        # print(seat.strip("\n") + ': row ' + str(row) + ', column ' + str(col) + ', seat Id ' + str(seat_number) + '.')
+        if seat_number > highest_seat_number:
+            highest_seat_number = seat_number
+    print(highest_seat_number)
